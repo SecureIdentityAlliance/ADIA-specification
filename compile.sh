f3e9e2c9dd7e00565c184d826d4b60d2a362e047
Kiran Addepalli <kiran@digitaltrust.net>
2021-02-10 18:26:21 -0500

    Modified to use without installing any dependencies including Bikeshed.

--- compile.sh
+++ compile.sh
@@ -7,8 +7,10 @@ set -x
 set -e
 
 # Iterate through the list of Bikeshed files and create the HTML files
+# Execute without installing Bikeshed!!
 for i in *.bs; do
-    bikeshed --print=plain spec -f "$i"
+    # bikeshed --print=plain spec -f "$i"
+    curl https://api.csswg.org/bikeshed/ -F file=@"$i" -F force=1 > "$i".html
 done
 
 # Create a fresh directory 'out'
