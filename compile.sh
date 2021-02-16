68a235d49217b0d08cb960b42e1d1f34275f7b52
Kiran Addepalli <kiran@digitaltrust.net>
2021-02-16 16:07:51 -0500

    Updated CI/CD

--- compile.sh
+++ compile.sh
@@ -6,20 +6,36 @@ set -x
 # Exit with nonzero exit code if anything fails
 set -e
 
+# Remove and create a fresh directory for output distribution
+rm -Rf dist
+mkdir -p dist
+
+
 # Iterate through the list of Bikeshed files and create the HTML files
 # Execute without installing Bikeshed!!
 for i in *.bs; do
-    # bikeshed --print=plain spec -f "$i"
-    curl https://api.csswg.org/bikeshed/ -F file=@"$i" -F force=1 > "${i%.*}".html
+    bikeshed --print=plain spec -f "$i"
+    # curl https://api.csswg.org/bikeshed/ -F file=@"$i" -F force=1 > "${i%.*}".html
 done
 
-# Create a fresh directory 'out'
-rm -Rf dist
-mkdir -p dist
 
 # Move all the generated HTML files to the output directory
 
 if [ -d dist ]; then
     mv *.html dist
     cp resources/index.html dist
+fi
+
+# Copy static resources - images, sequence diarams and resources to dist
+
+if [ -d resources ]; then
+  cp -R resources dist
+fi
+
+if [ -d sequence-diagrams ]; then
+  cp -R sequence-diagrams dist
+fi
+
+if [ -d images ]; then
+  cp -R images dist
 fi
